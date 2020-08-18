34cdd654c14d80d4a18d12d659e39173c78ee167
Jungwon Oh <jungwon.oh87@gmail.com>
2020-08-17 21:11:24 -0600

    project created and read component added

---
+++ src/components/Read.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Read component</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/data/index.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    author: "author 1",
+    title: "title 1",
+    content: "content 1"
+  },
+  {
+    author: "author 2",
+    title: "title 2",
+    content: "content 2"
+  },
+  {
+    author: "author 3",
+    title: "title 3",
+    content: "content 3"
+  }
+];
